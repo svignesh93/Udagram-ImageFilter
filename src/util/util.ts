991ff08c4c83de58ff8a1df029afaa57ee4999b9
svignesh93 <vignesh.vic727@gmail.com>
2022-07-25 20:05:06 +0530

    Updated filter image promise

--- src/util/util.ts
+++ src/util/util.ts
@@ -10,20 +10,19 @@ import Jimp from 'jimp';
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
-    try {
-      const photo = await Jimp.read(inputURL);
+    await Jimp.read(inputURL).then((photo) => {
       const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + photo.getExtension();
-      await photo
+        "/tmp/filtered." + Math.floor(Math.random() * 2000) + '.jpg';
+      photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
+        .write(__dirname + outpath, () => {
           resolve(__dirname + outpath);
         });
-    } catch (error) {
+    }).catch((error) => {
       reject(error);
-    }
+    });
   });
 }
 
